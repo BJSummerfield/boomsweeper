40f10d97eeb2ee912ca10e65c5af18ce87bae1dd
Brian Summerfield <brianjsummerfield@gmail.com>
2019-06-23 15:01:47 -0500

    add floodfill

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,11 +1,12 @@
 import Grid from './views/grid.js'
 import Board from './models/board.js'
 
-const GRID_SIZE = 16
-const board = new Board(GRID_SIZE, 30);
+const GRID_SIZE = 8
+const board = new Board(GRID_SIZE, 10);
 const cells = document.querySelector('.cell-container');
 const grid = new Grid({ element: cells, board })
 grid.boardSetup()
+
 
 
 
--- public/javascript/views/grid.js
+++ public/javascript/views/grid.js
@@ -7,7 +7,12 @@ class Grid {
 
 	addListeners() {
 		this.element.addEventListener('click', () => {
-			this.reveal(event.target.classList[0])
+			if(this.board[event.target.classList[0]].value == 'X') {
+				this.gameOver(event.target.classList[0])
+			} else {
+				this.reveal(event.target.classList[0])
+			}
+			this.render()
 		})
 	}
 
@@ -30,17 +35,17 @@ class Grid {
 	// reveal needs to be expanded to rest of game
 	reveal(index) {
 		this.board[index].reveal = true
-		this.render()
+		if (this.board[index].value == '') {
+			this.floodFill(index)
+		}
 	}
 
 	render () {
+		console.log('render')
 		var counter = 0
 		var element = this.element
 		var template = this.template
 		this.board.forEach(function(cell) {
-			if (cell.reveal == false) {
-				element.childNodes[counter].className = counter + ' hidden'
-			}
 			if (cell.reveal == true) { 
 				element.childNodes[counter].className = counter + ' reveal'
 				element.childNodes[counter].innerHTML = template(cell.value)
@@ -48,6 +53,41 @@ class Grid {
 			counter++
 		})
 	}
-}
 
+	gameOver(index) {
+		console.log('go')
+		this.board.forEach(function(cell) {
+			cell.reveal = true
+		})
+	}
+	
+	floodFill(index) {
+		var i = Math.floor(index / 8)
+		var j = index % 8
+		var size = 8
+		var cells = []
+  	
+  	// splice board into coordinates
+  	while (this.board.length > 0) {
+  		cells.push(this.board.splice(0, 8))
+  	}
+  	this.board = cells.flat()	
+			// X axis offsets
+			for (var xoff = -1; xoff <=1; xoff++) {
+				var x = i + xoff
+				if (x < 0 || x >= size) continue
+					
+				// Y axis offsets
+				for (var yoff = -1; yoff <=1; yoff++) {
+					var y = j + yoff
+					if (y < 0 || y >= size) continue
+					
+				if (cells[x][y].reveal == false) {
+					var flatten = (x * 8) + y
+					this.reveal(flatten)
+				}
+			}
+		}			
+	}
+}					
 export default Grid
